999ca554b7e6e7f71a99090777edf0d16581d12a
Paul Anderson <pauldevop@gmail.com>
2024-02-20 18:52:21 -0300

    H2 Database

--- backend/src/test/java/com/devsuperior/dscatalog/DscatalogApplicationTests.java
+++ backend/src/test/java/com/devsuperior/dscatalog/DscatalogApplicationTests.java
@@ -1,12 +1,7 @@
 package com.devsuperior.dscatalog;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class DscatalogApplicationTests {
 
-	@Test
 	void contextLoads() {
 	}
 
